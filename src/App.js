e0f7b2e498609d45b84721e7005b9cc3cf854fc1
Raghu <konidelaraghu@gmail.com>
2020-09-12 20:35:38 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom'
+import { PostsList } from './features/posts/PostsList'
+
+import { Navbar } from './app/Navbar'
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <div className="App">
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <React.Fragment>
+                <PostsList />
+              </React.Fragment>
+            )}
+          />
+          <Redirect to="/" />
+        </Switch>
+      </div>
+    </Router>
+  )
+}
+
+export default App
---
+++ src/app/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <nav>
+      <section>
+        <h1>Posting posts using Redux</h1>
+
+        <div className="navContent">
+          <div className="navLinks"></div>
+        </div>
+      </section>
+    </nav>
+  )
+}
